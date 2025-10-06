d1b0552dfa36e45bc2d7a5c4d81a6fc99378f093
Ernesto Rios <ernestorios@outlook.com.pe>
2025-10-06 13:48:09 -0500

    Update create tables

--- lab06/01_ddl_create_tables.sql
+++ lab06/01_ddl_create_tables.sql
@@ -2,9 +2,12 @@ USE testing;
 
 DROP TABLE IF EXISTS parking;
 
-CREATE TABLE parking (
-	SystemCodeNumber VARCHAR(100),
-	Capacity INT,
-	Occupancy INT,
-	LastUpdated DATETIME
-);
+IF OBJECT_ID(N'dbo.parking', N'U') IS NULL
+BEGIN
+	CREATE TABLE parking (
+		SystemCodeNumber VARCHAR(100),
+		Capacity INT,
+		Occupancy INT,
+		LastUpdated DATETIME
+	);
+END;
